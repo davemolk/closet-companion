821a953b976955a4a8f8faecdc13cfdffa73808f
davemolk <davemolk@gmail.com>
2021-07-28 10:21:35 -0600

    make url, html, view

--- user/urls.py
+++ user/urls.py
@@ -6,5 +6,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('login/', views.loginUser, name='login'),
+    path('logout', views.logoutUser, name='logout'),
+    path('signup/', views.signupUser, name='signup'),
 
 ]
--- user/views.py
+++ user/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from .models import Profile,
 
 # Create your views here.
 
@@ -7,4 +10,23 @@ def index(request):
     return render(request, 'index.html')
 
 def loginUser(request):
-    return HttpResponse('hi')
+    pass
+
+
+def signupUser(request):
+    form = UserCreationForm()
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            #messages.success(request, 'User account created!')
+            login(request, user)
+            return redirect('account')
+        else:
+            #messages.error(request, 'An error has occurred during registration')
+            return redirect('signup')
+    context = {'form': form}
+    return render(request, 'user/signup.html', context)
