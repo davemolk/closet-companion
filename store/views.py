0406a39e9599001f2c6c03a1ec538675329f782e
davemolk <davemolk@gmail.com>
2021-07-31 19:57:31 -0600

    basic store

--- store/views.py
+++ store/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+import json
+from .models import Order, OrderItem, ShippingAddress
+from user.models import Profile
+from wardrobe.models import Item
+import datetime
+from django.contrib.auth.decorators import login_required
+# from .utils import cookieCart, cartData, guestOrder
 
 # Create your views here.
+
+@login_required(login_url="login")
+def store(request):
+    items = Item.objects.filter(sell=True)
+    context = {'items': items}
+    return render(request, 'store/store.html', context)
+    
