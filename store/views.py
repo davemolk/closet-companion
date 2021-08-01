c98a61e3479d1bdb6fa5018990c65b9725ca6cfc
davemolk <davemolk@gmail.com>
2021-07-31 20:23:41 -0600

    running into db error

--- store/views.py
+++ store/views.py
@@ -6,12 +6,15 @@ from user.models import Profile
 from wardrobe.models import Item
 import datetime
 from django.contrib.auth.decorators import login_required
-# from .utils import cookieCart, cartData, guestOrder
+from .utils import cartData
 
 # Create your views here.
 
 @login_required(login_url="login")
 def store(request):
+    data = cartData(request)
+    cartItems = data['cartItems']
+
     items = Item.objects.filter(sell=True)
     context = {'items': items}
     return render(request, 'store/store.html', context)
